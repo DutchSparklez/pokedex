6130123d5b3f07d75975d4826c53eabb932270bc
DutchSparklez <wschaijk@outlook.com>
2022-10-28 16:08:10 +0200

    Text to Speech & Swipe Detection

--- pokedex/pokedex/MainActivity.cs
+++ pokedex/pokedex/MainActivity.cs
@@ -8,6 +8,11 @@ using Newtonsoft.Json;
 using pokedex.Models;
 using System.Collections.Generic;
 using System.IO;
+using static Android.Views.GestureDetector;
+using System.Runtime.InteropServices;
+using AndroidX.Core.View;
+using System;
+using Android.Widget;
 
 namespace pokedex
 {
@@ -15,9 +20,10 @@ namespace pokedex
     /// The main activity: the starting point of the application
     /// </summary>
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, ScreenOrientation = ScreenOrientation.Landscape)]
-    public class MainActivity : AppCompatActivity
+    public class MainActivity : Activity
     {
         private List<Pokemon> pokemonData = new List<Pokemon>();
+        private GestureDetectorCompat gestureDetector;
 
         /// <summary>
         /// This function creates the main activity and places it on the screen.
@@ -37,6 +43,21 @@ namespace pokedex
 
             // Display the activity on screen
             SetContentView(Resource.Layout.activity_main);
+
+            // Enable Gesture Detection
+            gestureDetector = new GestureDetectorCompat(this, new MyGestureListener());
+        }
+
+        /// <summary>
+        /// This function directs touch events to the MyGestureListener class.
+        /// </summary>
+        /// <param name="e">A motion event, contains information on the interaction with the screen</param>
+        /// <returns>A boolean that tells whether a touch event has been registered and handled</returns>
+        public override bool OnTouchEvent(MotionEvent e) 
+        {
+            // Direct the touch event to MyGestureListeren
+            gestureDetector.OnTouchEvent(e);
+            return base.OnTouchEvent(e);
         }
 
         /// <summary>
@@ -56,14 +77,82 @@ namespace pokedex
 
             // Convert the data to a list of Pokémon objects
             pokemonData = JsonConvert.DeserializeObject<List<Pokemon>>(json);
+
+            // Save the data of a single pokemon
+            Pokemon test = pokemonData[0];
+
+            // Let the app speak out the details of the pokemon
+            test.SayDetails();
         }
 
         // Build in function to manage android permission
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
         {
             // Set all the required permission
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
-	}
+        
+        /// <summary>
+        /// We need a MyGestureClass because of the way Android handles touch events.
+        /// If we didn't use this, we would need to implement every single possible swipe action.
+        /// </summary>
+        internal class MyGestureListener : SimpleOnGestureListener
+        {
+            // Create a constant that is used to calculate whether a swipe has been made.
+            private readonly int MinimumVelocity = 200;
+
+            /// <summary>
+            /// This function checks whether the user has swiped across the screen, then determines the direction of said swipe.
+            /// </summary>
+            /// <param name="differenceX">The distance between the start and endpoint of the swipe on the X-axis</param>
+            /// <param name="differenceY">The distance between the start and endpoint of the swipe on the Y-axis</param>
+            /// <param name="velocityX">The speed of the swipe across the X-axis</param>
+            /// <param name="velocityY">The speed of the swipe across the Y-axis</param>
+            /// <returns>A string with the direction if a swipe has been detected, an empty string otherwise</returns>
+            private string GetSwipeDirection(float differenceX, float differenceY, float velocityX, float velocityY)
+            {
+                // Check whether the motion counts as a horizontal swipe
+                if (Math.Abs(differenceX) > Math.Abs(differenceY) && Math.Abs(differenceX) > MinimumVelocity && Math.Abs(velocityX) > MinimumVelocity)
+                {
+                    // Return the direction of the swipe
+                    return differenceX > 0 ? "Right" : "Left";
+                }
+                
+                // Check whether the motion counts as a vertical swipe
+                if (Math.Abs(differenceY) > MinimumVelocity && Math.Abs(velocityY) > MinimumVelocity)
+                {
+                    // Return the direction of the swipe
+                    return differenceY > 0 ? "Down" : "Up";
+                }
+
+                // No valid swipe was detected, return an empty string
+                return "";
+            }
+
+            /// <summary>
+            /// The function that get's called automatically when the user flings their finger across the screen.
+            /// </summary>
+            /// <param name="e1">The startingpoint of the swipe</param>
+            /// <param name="e2">The endpoint of the swipe</param>
+            /// <param name="velocityX">The speed of the swipe across the X-axis</param>
+            /// <param name="velocityY">The speed of the swipe across the Y-axis</param>
+            /// <returns>True if the event was handled, false it wasn't</returns>
+            public override bool OnFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY)
+            {
+                // Get the direction of the swipe, this becomes an empty string if the movement is not a valid swipe.
+                string direction = GetSwipeDirection(e2.GetX() - e1.GetX(), e2.GetY() - e1.GetY(), velocityX, velocityY);
+
+                // Determine the action based on the direction
+                switch (direction)
+                {
+                    case "Right": Console.WriteLine(direction); return true;
+                    case "Left": Console.WriteLine(direction); return true;
+                    case "Up": Console.WriteLine(direction); return true;
+                    case "Down": Console.WriteLine(direction); return true;
+                    default: return false;
+                }
+            }
+        }
+    }
 }
--- pokedex/pokedex/Models/Pokemon.cs
+++ pokedex/pokedex/Models/Pokemon.cs
@@ -1,4 +1,10 @@
-﻿namespace pokedex.Models
+﻿using Android.Content;
+using Java.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+
+namespace pokedex.Models
 {
     /// <summary>
     /// The object template that hold the data of a single pokemon.
@@ -6,14 +12,58 @@
     /// </summary>
     class Pokemon
     {
-        public string Number { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public string Category { get; set; }
-        public string ImageUrl { get; set; }
-        public string Length { get; set; }
-        public string Weight { get; set; }
-        public string[] Abilities { get; set; }
-        public string[] Typing { get; set; }
+        // The data of the Pokemon
+        public string Number;
+        public string Name;
+        public string Description;
+        public string Category;
+        public string ImageUrl;
+        public string Length;
+        public string Weight;
+        public string[] Abilities;
+        public string[] Typing;
+
+        /// <summary>
+        /// This function uses the Text To Speech engine of the phone to say a subset of the Pokemon's data.
+        /// This function fails if Text To Speech hasn't been settup at the phone.
+        /// </summary>
+        public async void SayDetails()
+        {
+            // Create the string that will be spoken and say it
+            await TextToSpeech.SpeakAsync($"{Name}. {GetCategoryString()}. {GetTypingString()}. {Description}");
+        }
+
+        /// <summary>
+        /// This function turns the category of the Pokemon into the desired format for display and announcing.
+        /// </summary>
+        /// <returns>A string in the following format: [The {Category} Pokémon]</returns>
+        public string GetCategoryString()
+        {
+            // Return the string
+            return $"The {Category} Pokémon";
+        }
+
+        /// <summary>
+        /// This function turns the typing of the Pokemon into the desired format for displaying and announcing.
+        /// It forms a grammatically correct sentence.
+        /// </summary>
+        /// <returns>A string in the following format: [A(n) {Type1} (and {Type2}) type]</returns>
+        public string GetTypingString()
+        {
+            // Create a list of vowels, used to check whether we need A or An at the start of our output
+            char[] vowels = { 'a', 'e', 'o', 'u', 'i' };
+
+            // Start the output
+            string output = "A";
+
+            // Determine whether we need to start the output with A or An
+            output += vowels.Contains(Typing[0].ToLower().First()) ? "n " : " ";
+
+            // Add the types to the sentence
+            output += Typing.Length == 2 ? $"{Typing[0]} and {Typing[1]}" : $"{Typing[0]}";
+
+            // End the sentence and return the result
+            return output + " type";
+        }
     }
 }
